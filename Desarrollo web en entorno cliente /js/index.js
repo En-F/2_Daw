784cd8a1a9c77b5d48af9878f478d31ac6a68b82
Enrique <enriquefranco@iesdonana.com>
2025-07-18 19:38:04 +0200

    Actualizacion del inicio de javascript

---
+++ Desarrollo web en entorno cliente /js/index.js
@@ -0,0 +1,13 @@
+/*
+Variables el let y el var se pueden modificar luego de la creacion de la variable 
+pero a la hora de imprimirlo por consola el var lo inicializa ocupando espacio en memoria
+, mientras que el let se crea en memoria cuando yo la defino, el valor const es constante 
+y no se puede modificar su valor  
+*/
+console.log(vivo)
+let edad=18;
+edad=19;
+
+var vivo=true;
+vivo=false;
+const NOMBRE="Jab;"
